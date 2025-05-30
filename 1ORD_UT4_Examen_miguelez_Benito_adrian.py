5dc79e21bb0e15e025c886e95a382ec5bcdcde98
Adrian Miguelez Benito <amiguelezb@gmail.com>
2025-05-30 16:15:03 +0200

    3 (2). Correcion de la clase dispositivo, donde se añaden los valores comunes y se implementa correctamente la funcion mostrar_info con el metodo abstracto implementado

--- 1ORD_UT4_Examen_miguelez_Benito_adrian.py
+++ 1ORD_UT4_Examen_miguelez_Benito_adrian.py
@@ -1,34 +1,36 @@
 # Clases para representar dispositivos
 from abc import ABC, abstractmethod
 
-class Dispositivo():
-    def __init__(self):
-        pass
-
-    @abstractmethod    
-    def mostrar_info():
-        pass
-
-class portatil:
+class Dispositivo(ABC):
     def __init__(self, marca, procesador, ram):
         self.marca = marca  # marca
         self.procesador = procesador  # procesador
         self.ram = ram
 
-    def mostrar_portatil(self):
+    @abstractmethod    
+    def mostrar_info():
+        pass
+
+class portatil():
+    def __init__(self, marca, procesador, ram):
+        self.marca = marca  # marca
+        self.procesador = procesador  # procesador
+        self.ram = ram
+
+    def mostrar_info(self):
         print("Dispositivo: Portátil")
         print(f"Marca: {self.marca}")
         print(f"Procesador: {self.procesador}")
         print(f"RAM: {self.ram} GB")
 
 
-class movil:
+class movil():
     def __init__(self, marca, procesador, ram):
         self.marca = marca  # marca
         self.procesador = procesador  # procesador
         self.ram = ram
 
-    def mostrar_movil(self):
+    def mostrar_info(self):
         print("Dispositivo: Móvil")
         print(f"Marca: {self.marca}")
         print(f"Procesador: {self.procesador}")
@@ -41,10 +43,10 @@ def main():
     dispositivo_2 = movil("Samsung", "Snapdragon", 8)
 
     print("== Información de dispositivos ==")
-    dispositivo_1.mostrar_portatil()
+    dispositivo_1.mostrar_info()
     print("------------------------")
-    dispositivo_2.mostrar_movil()
-+
+    dispositivo_2.mostrar_info()
+
 
 if __name__ == "__main__":
     main()
