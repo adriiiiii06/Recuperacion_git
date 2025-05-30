e0058c9eb30d51636c6f2dbfd3d0dcd4d3e7f633
Adrian Miguelez Benito <amiguelezb@gmail.com>
2025-05-30 16:45:25 +0200

    6.Se ha modificado la funcion main para  preguntar al usuario si queire crear un dispositivo, y en tal caso se le pregunta cual y los datos correspondientes de dicho dispositivo para crearlo, posteriormente hacemos que mustre la informacion

--- 1ORD_UT4_Examen_miguelez_Benito_adrian.py
+++ 1ORD_UT4_Examen_miguelez_Benito_adrian.py
@@ -29,22 +29,39 @@ class movil(Dispositivo):
         print(f"RAM: {self.ram} GB")
 
 class creacion_dispositivos:
-    def utilidades():
-        dispositivo_1 = portatil("Dell", "i7", 16)
-        dispositivo_2 = movil("Samsung", "Snapdragon", 8)
-        return dispositivo_1,dispositivo_2
+    def utilidades(respuesta, marca, procesador, ram):
+        if respuesta.lower() == "portatil":
+            dispositivo_1 = portatil(marca, procesador, ram)
+            return dispositivo_1
+        if respuesta.lower() == "movil":  
+            dispositivo_2 = movil(marca, procesador, ram)
+            return dispositivo_2
 
 # Función principal
 def main():
-    dispositivo_1, dispositivo_2 = creacion_dispositivos.utilidades()
+    while True:
+        pregunta = input("Deseas registrar un dispositivo: ")
+        if pregunta == "si":
+            respuesta = input("Que tipo de dispositivo deseas registrar (portatil o movil): ")
+            if respuesta.lower() == "portatil":
+                marca = input("Que tipo de marca es: ")
+                procesador = input("Que tipo de procesador es: ")
+                ram = input("Que tipo de ram tiene: ") 
+                dispositivo_1 = creacion_dispositivos.utilidades(respuesta, marca, procesador, ram)
+            if respuesta.lower() == "movil":  
+                marca_1 = input("Que tipo de marca es: ")
+                procesador_1 = input("Que tipo de procesador es: ")
+                ram_1 = input("Que tipo de ram tiene: ")
+                dispositivo_2 = creacion_dispositivos.utilidades(respuesta, marca_1, procesador_1, ram_1)
 
-    print("== Información de dispositivos ==")
-    dispositivo_1.mostrar_info()
-    print("------------------------")
-    dispositivo_2.mostrar_info()
 
-
-
+        elif pregunta == "no":
+            print("== Información de dispositivos ==")
+            dispositivo_1.mostrar_info()
+            print("------------------------")
+            dispositivo_2.mostrar_info()
+            print("Saliendo del programa...")
+            break
 
 if __name__ == "__main__":
     main()
